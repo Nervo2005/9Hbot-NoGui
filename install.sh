6bf249597328c55783c52dbe0a63e21549322c44
MauroS5 <52974793+MauroS5@users.noreply.github.com>
2020-12-26 18:44:47 +0100

    0.0.2

--- install.sh
+++ install.sh
@@ -17,15 +17,16 @@ else
             new=2
             ;;
     esac
+    apt-get install -y xvfb wget libgtk-3-0 libgbm-dev libatspi2.0-0 libatomic1
     wget https://www.dropbox.com/s/5jnaavaken3vo6r/9hits-bot-linux-x64.tar.bz2
     tar -xjvf 9hits-bot-linux-x64.tar.bz2
     mv /root/9Hits-bot/9hits-bot-linux-x64 /root/9Hits-bot/9HitsViewer-bot
-    rm -rf 9hits-bot-linux-x64.tar.bz2
+    rm -rf 9hits-bot-linux-x64.tar.bz2 9hits-bot-linux-x64
     if [ new == "2" ]; then
         rm -rf 9HitsViewer-bot/config
-        mv /root/config 9HitsViewer-bot/config
+        cp /root/config 9HitsViewer-bot/config
     fi
-    mv /root/9Hbot-NoGui/* /root/9Hits/
+    mv /root/9Hbot-NoGui/* /root/9Hits-bot/
     chmod +x -R /root/9Hits-bot/
     rm -r 9Hbot-NoGui
     crontab crontab
--- kill.sh
+++ kill.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kilall 9hbot 9hbrowser
+kilall 9hbot 9hbrowser 9htl
